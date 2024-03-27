2cebb35e474e517bfd01be66232ff6ce8c38da29
chrysanthemum <asingiser@gmail.com>
2024-03-27 09:52:01 -0400

    start with function for api call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
+import OpenAI from 'openai';
+import 'dotenv/config';
+
+
+export async function getServerSideProps() {
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+  const response = await openai.completions.create({
+    engine: 'text-davinci-003',
+    prompt: 'Once upon a time...'
+  });
+
+  const story = await response.choices[0].text;
+
+  if (!story) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { story }, // will be passed to the page component as props
+  }
+}
+
 export default function Home() {
   return (
     <div>
