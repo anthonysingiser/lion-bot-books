d5d4d6e55146b9054488a09b519a62854ab81154
Anthony <asingiser@gmail.com>
2024-04-13 22:18:34 -0400

    chnage componenet to display all pictures and images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,32 @@
 import Image from 'next/image';
+import { useState } from 'react';
 import createBook from '../utils/api';
 
 export async function getServerSideProps() {
 
     try {
       const book = await createBook();
-      return { props: { story: book.story, image: book.image } };
+      return { props: {book} };
     } catch (error) {
       console.error(error);
       return { props: { story: '', image: '' } };
     }
 }
 
-export default function Home({ story, image }: { story: string, image: string }) {
+export default function Home({ book }: { book: { story: string, image: string }[] }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleClick = () => {
+    if (currentIndex < book.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
   return (
     <div>
-      <Image src={image} alt="book illustration" width={600} height={400}/>
-      <h2>{story}.</h2>
+      <Image src={book[currentIndex].image} alt="book illustration" width={600} height={400}/>
+      <h2>{book[currentIndex].story}.</h2>
+      <button onClick={handleClick}>Next</button>
     </div>
   );
 }
