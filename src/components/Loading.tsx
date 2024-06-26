6e2cf37fca51439d5838ef720a605462323981c7
Anthony Singiser <asingiser@gmail.com>
2024-06-26 12:02:35 -0400

    correct padding issue, improve readablity, accessibility improvements

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,8 @@
 export default function Loading() {
-    return (
-      <div className="flex justify-center items-center min-h-screen">
-        <div className="animate-bounce h-12 w-12 bg-purple-500 p 3">
-          making book...
-        </div>
-      </div>
-    );
-  }
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen" aria-live="polite">
+      <div className="animate-bounce h-12 w-12 bg-purple-500 p-3 rounded-full"></div>
+      <p className="mt-4 text-lg text-purple-700">Making book...</p>
+    </div>
+  );
+}
