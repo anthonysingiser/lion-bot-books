e93ea41c62bf1896de9e590414c4ab92c3ab9e7e
Anthony Singiser <asingiser@gmail.com>
2024-06-28 11:13:29 -0400

    center image box vertically and horizontally, do the same for the page box

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -7,13 +7,15 @@ type BookProps = {
 
 export default function Book({ book, currentIndex }: BookProps) {
   return (
-    <div>
-      <Image
-        src={book[currentIndex].image}
-        alt="book illustration"
-        width={600}
-        height={400}
-      />
+    <div className= "flex flex-col items-center">
+      <div className="flex justify-center w-full">
+        <Image
+          src={book[currentIndex].image}
+          alt="book illustration"
+          width={600}
+          height={400}
+        />
+      </div>
  <h2 className="text-4xl text-center font-bold text-blue-500 mt-8">
   {book[currentIndex]?.story && book[currentIndex].story.trim() !== "" ? book[currentIndex].story : "THE END"}.
 </h2>
