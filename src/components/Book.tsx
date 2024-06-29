feefb1454d30707238a7851a331dcbf331a8fea5
Anthony Singiser <asingiser@gmail.com>
2024-06-29 09:34:54 -0400

    set Book interface, initialize currentBook in Book component and return 'content not available' if empty

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,11 +1,20 @@
 import Image from 'next/image';
 
-type BookProps = {
-    book: { story: string, image: string }[];
+interface BookProps {
+    book: { 
+      story?: string, 
+      image: string 
+    }[];
     currentIndex: number;
   };
 
 export default function Book({ book, currentIndex }: BookProps) {
+  const currentBook = book[currentIndex];
+
+  if (!currentBook) {
+    return <div>Book content is not available.</div>;
+  }
+
   return (
     <div className= "flex flex-col items-center">
       <div className="flex justify-center w-full">
@@ -17,7 +26,7 @@ export default function Book({ book, currentIndex }: BookProps) {
         />
       </div>
  <h2 className="text-4xl text-center font-bold text-blue-500 mt-8">
-  {book[currentIndex]?.story && book[currentIndex].story.trim() !== "" ? book[currentIndex].story : "THE END"}.
+  {currentBook.story && currentBook.story.trim() !== "" ? currentBook.story : "THE END"}.
 </h2>
     </div>
   );
