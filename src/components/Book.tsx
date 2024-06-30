098ac2f2a735f4b6dac73c0a0b5649d4c115038d
Anthony Singiser <asingiser@gmail.com>
2024-06-30 12:43:09 -0400

    update image rendering to track loading status of images

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import { useState } from 'react';
+
 
 interface BookProps {
     book: { 
@@ -10,14 +12,20 @@ interface BookProps {
 
 export default function Book({ book, currentIndex }: BookProps) {
   const currentBook = book[currentIndex];
+  const [isLoading, setIsLoading] = useState(true)
 
   if (!currentBook) {
     return <div>Book content is not available.</div>;
   }
 
+  const handleImageLoad = () => {
+    setIsLoading(false); // Set loading to false when image is loaded
+  };
+
   return (
     <div className= "flex flex-col items-center">
       <div className="flex justify-center w-full">
+      {isLoading && <div>Loading image...</div>}
         <Image
           src={book[currentIndex].image}
           alt="book illustration"
