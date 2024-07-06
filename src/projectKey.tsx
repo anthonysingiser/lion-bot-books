958c7e72d4f66351e30aefddad6cc62189e2f98a
Anthony Singiser <asingiser@gmail.com>
2024-07-06 09:13:49 -0400

    change env key for Next.js, before it was for React

--- src/projectKey.tsx
+++ src/projectKey.tsx
@@ -2,7 +2,8 @@ import 'dotenv/config';
 import { OpenAI } from 'openai';
 
 const openai = new OpenAI({
-    apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+    dangerouslyAllowBrowser: true
 });
 
 export { openai };
